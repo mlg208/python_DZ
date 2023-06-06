26bd9f845f0018b22840739350e85219be9ae42b
mlg208 <134071268+mlg208@users.noreply.github.com>
2023-06-06 12:29:50 +0600

    Add files via upload

---
+++ Новая папка/1.py
@@ -0,0 +1,5 @@
+total_sales = float(input("Введите общую сумму продаж за месяц: "))
+base_salary = 250  
+commission = 0.1 * total_sales 
+salary = base_salary + commission  
+print("Зарплата работника составляет:", salary)
