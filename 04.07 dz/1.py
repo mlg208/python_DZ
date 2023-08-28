ba880bc5ff3ab5c0a371f6f1ad9f04938e1f25ec
mlg208 <134071268+mlg208@users.noreply.github.com>
2023-08-28 06:10:57 +0600

    Add files via upload

---
+++ 04.07 dz/1.py
@@ -0,0 +1,29 @@
+def extract_emails(text):
+    emails = []
+    i = 0
+    while i < len(text):
+        if text[i] == "@":
+            start = i - 1
+            while start >= 0 and (text[start].isalnum() or text[start] in "-._"):
+                start -= 1
+            start += 1
+
+            end = i + 1
+            while end < len(text) and (text[end].isalnum() or text[end] in "-._"):
+                end += 1
+
+            emails.append(text[start:end])
+            i = end
+        else:
+            i += 1
+    return emails
+
+
+with open("input.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+email_addresses = extract_emails(text)
+email_string = ", ".join(email_addresses)
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write(email_string)
+
+print("Адреса электронной почты извлечены и сохранены в output.txt")
---
+++ 04.07 dz/2.py
@@ -0,0 +1,27 @@
+def syllabify(word):
+    vowels = 'aeiouаеёиоуыэюя'
+    syllables = []
+    current_syllable = ""
+
+    for char in word:
+        if char.lower() in vowels:
+            current_syllable += char
+            syllables.append(current_syllable)
+            current_syllable = ""
+        else:
+            current_syllable += char
+
+    if current_syllable:
+        syllables.append(current_syllable)
+
+    return syllables
+
+with open("input.txt", "r", encoding="utf-8") as f:
+    words = f.read().splitlines()
+with open("output.txt", "w", encoding="utf-8") as f:
+    for word in words:
+        syllables = syllabify(word)
+        syllable_str = "-".join(syllables)
+        f.write(syllable_str + "\n")
+
+print("Слова разбиты на слоги и сохранены в output.txt")
