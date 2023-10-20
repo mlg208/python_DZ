3a383f9ae410762167e38b71e71a08430c64bee2
mlg208 <134071268+mlg208@users.noreply.github.com>
2023-10-20 18:22:57 +0600

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,16 +1,67 @@
-def encrypt(text):
-    encrypted_text = ''
-    for char in text:
-        if char.isalpha():
-            if char.islower():
-                encrypted_text += chr((ord(char) - ord('а') + 1) % 32 + ord('а'))
-            elif char.isupper():
-                encrypted_text += chr((ord(char) - ord('А') + 1) % 32 + ord('А'))
-        else:
-            encrypted_text += char
-    return encrypted_text
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLabel
 
-input_text = "Мощная армия дуболомов Урфина Джюса уже третий день осаждает Изумрудный город."
-encrypted_text = encrypt(input_text)
-with open("encrypted_message.txt", "w", encoding="utf-8") as f:
-    f.write(encrypted_text)
+class Calculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Простой калькулятор")
+        self.setGeometry(100, 100, 300, 400)
+
+        layout = QVBoxLayout()
+
+        self.result_label = QLabel("0") 
+        layout.addWidget(self.result_label)
+
+        buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', 'C', '=', '+',
+        ]
+
+        grid_layout = QGridLayout()
+        row, col = 1, 0
+
+        for button_text in buttons:
+            button = QPushButton(button_text)
+            button.clicked.connect(self.on_button_click)
+            grid_layout.addWidget(button, row, col)
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
+
+        layout.addLayout(grid_layout)
+        self.setLayout(layout)
+
+    def on_button_click(self):
+        clicked_button = self.sender()
+        if clicked_button:
+            button_text = clicked_button.text()
+            current_text = self.result_label.text() 
+
+            if button_text == '=':
+                try:
+                    result = str(eval(current_text))
+                    self.result_label.setText(result)
+                except Exception:
+                    self.result_label.setText("Ошибка")
+            elif button_text == 'C':
+                self.result_label.setText('0')
+            else:
+                if current_text == '0':
+                    self.result_label.setText(button_text)
+                else:
+                    self.result_label.setText(current_text + button_text)
+
+def main():
+    app = QApplication(sys.argv)
+    calc = Calculator()
+    calc.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
